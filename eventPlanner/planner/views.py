db34854f9b9055d9e90c700af23d1f8cf2bea31c
IsaaMaster <songisaac17@gmail.com>
2023-10-02 08:26:18 -0700

    blank html pages and views to handle GET requests

--- eventPlanner/planner/views.py
+++ eventPlanner/planner/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def events(request):
+    return render(request, 'browseEvents.html')
+
+
+def event(request):
+
+    return render(request, 'eventDetail.html')
