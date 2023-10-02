2420c7332ba07f60dbe805cf3b241bd0d4cb14ee
IsaaMaster <85504364+IsaaMaster@users.noreply.github.com>
2023-10-02 12:51:00 -0700

    Intial Project Creation (#23)
    * Intial Project Creation
    
    * planer intial app creation
    
    * delete
    
    * Revert "delete"
    
    This reverts commit 9c38e8bdd6a45d5414384be48998d40b5c870132.
    
    * Revert "planer intial app creation"
    
    This reverts commit 158f77d26bdcaf7dcf33ded118cefb7945e1eb33.
    
    * Revert "Revert "planer intial app creation""
    
    This reverts commit 84322508fda85e07ad3646662ff9fb20e7dc5ad4.
    
    * blank html pages and views to handle GET requests

---
+++ eventPlanner/planner/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('events/', views.events, name = 'events'),
+
+    #this need to be changed later to event/<int:event_id>, but for now it is just eventDetail because we don't have the database setup yet
+    path('eventDetail', views.event, name = 'event'),  
+]
