fc749106396da8f87e462dd13461106cb1877707
IsaaMaster <85504364+IsaaMaster@users.noreply.github.com>
2023-10-03 17:00:16 -0700

    added event and task model (#24)

--- eventPlanner/planner/models.py
+++ eventPlanner/planner/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+#event class
+#descbribes an specifc event with a name, data, time and location
+class Event(models.Model):
+    name = models.CharField(max_length=250)
+    date = models.CharField(max_length=100) #Need to be updated to DateField in future release
+    time = models.CharField(max_length=100)  #Need to be updated to TimeField in future release
+    location = models.CharField(max_length=100)
+
+    """
+    Features to be added in future releases
+    description = models.CharField(max_length=1000) 
+    capacity = models.IntegerField() 
+    image = models.ImageField(upload_to='images/') 
+    """
+
+    def __str__(self):
+        return self.name
+    
+#task class
+#describes a task for a specific event
+class Task(models.Model):
+    name = models.CharField(max_length=250)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
--- eventPlanner/planner/views.py
+++ eventPlanner/planner/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-
+#home page view for the event planner
 def home(request):
     return render(request, 'home.html')
 
+#browse events view
 def events(request):
     return render(request, 'browseEvents.html')
 
-
+#event detail view: viewing a specific event
 def event(request):
-
     return render(request, 'eventDetail.html')
