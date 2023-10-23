7bbf80745c6680a8a5a3c6fc4e58f479bb518c09
aL pete <alpete@wifi-124-0-96-westmont.edu>
2023-10-23 15:22:46 -0700

    changes to POST createEvent form

--- eventPlanner/planner/forms.py
+++ eventPlanner/planner/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from .models import Event
 
-class CreateEventForm(forms.Form): 
+class CreateEventForm2(forms.Form): 
     event_name = forms.CharField(label="Event name", max_length=100)
     event_host = forms.CharField(label="Enter your name", max_length=100)
     event_location = forms.CharField(label="Location", max_length=100)
     event_date = forms.CharField(label="Date", max_length=100)
     event_time = forms.CharField(label="time", max_length=100)
     event_description = forms.CharField(label="Describe your event.", max_length=250)
+    
+    class Meta:
+        model = Event
+        exclude = ("user", )
