6b6bb2614fb0cd994d2bb4b50d6bb6c8e56347b5
magictwicks <49892992+magictwicks@users.noreply.github.com>
2023-11-13 16:09:43 -0800

    Sprint3 test cases (#93)

---
+++ eventPlanner/tests/SeleniumDriver.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ChromeOptions
+
+def user_login(driver):
+    # opens Login Page and logs in as the test user
+    driver.get("http://127.0.0.1:8000/login") 
+    driver.find_element(By.ID, "email").send_keys("test@gmail.com")
+    driver.find_element(By.ID, "password").send_keys("test")
+    driver.find_element(By.ID, "submit").click()
+# This defines a context manager for the Selenium WebDriver
+class ChromeDriver:
+    def __init__(self):
+        options = ChromeOptions()
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        options.add_argument("--headless=new")
+        options.add_argument("--start-maximized")
+        driver = webdriver.Chrome(options=options)
+        driver.implicitly_wait(4)
+        self.driver = driver
+
+    def __enter__(self):
+        return self.driver
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.driver.quit()
+
+chrome_driver = ChromeDriver
