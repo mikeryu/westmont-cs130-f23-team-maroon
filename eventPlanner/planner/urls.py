4d813604eda3d68d890d8c6cbd65dbaca2affed2
magictwicks <49892992+magictwicks@users.noreply.github.com>
2023-11-28 16:00:44 -0800

    My events page (#118)
    * added LICENSE and updated the README.md
    
    * added myevents page

--- eventPlanner/planner/urls.py
+++ eventPlanner/planner/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('events/', views.events, name = 'events'),
 
-    #this need to be changed later to event/<int:event_id>, but for now it is just eventDetail because we don't have the database setup yet
     path('event/<int:id>', views.event, name = 'event'),  
     path('createEvent', views.createEvent, name = 'createEvent'),
+    path('myEvents', views.myEvents, name = 'myEvents'),
     path('login', views.userLogin, name = 'loginPage'),
     path('signup', views.signup, name = 'signUp'),
     path('manageAccount', views.manageAccount, name='manageAccount'),
