e14f1cf86df16c3e72f01bcb9940fb4850a0786f
IsaaMaster <songisaac17@gmail.com>
2023-11-30 00:03:59 -0800

    merged and modified my events page

--- eventPlanner/eventPlanner/settings.py
+++ eventPlanner/eventPlanner/settings.py
@@ -130,6 +130,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_REDIRECT_URL = "events"
+LOGIN_REDIRECT_URL = "myEvents"
 LOGOUT_REDIRECT_URL = "home"
 LOGIN_URL = "loginPage"
