be81b57b470625188bb5d66c2c3cf4e508a332b2
magictwicks <49892992+magictwicks@users.noreply.github.com>
2023-12-02 15:33:35 -0800

    github actions: run tests on push to main (#114)
    * Create python-app.yml
    
    * changed file path for user tests
    
    * Update python-app.yml
    
    * Update python-app.yml
    
    * Update python-app.yml
    
    * Update python-app.yml
    
    * Update python-app.yml
    
    * Update python-app.yml

--- eventPlanner/tests/SeleniumDriver.py
+++ eventPlanner/tests/SeleniumDriver.py
@@ -6,7 +6,7 @@ from selenium.webdriver import ChromeOptions
 def user_login(driver):
     # opens Login Page and logs in as the test user
     driver.get("http://127.0.0.1:8000/login") 
-    driver.find_element(By.ID, "email").send_keys("test@gmail.com")
+    driver.find_element(By.ID, "username").send_keys("test")
     driver.find_element(By.ID, "password").send_keys("test")
     driver.find_element(By.ID, "submit").click()
 
