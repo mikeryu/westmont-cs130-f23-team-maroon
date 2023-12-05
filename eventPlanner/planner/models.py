b67f1fcff8bb8d4bb3fd579f1c742a5328a9a498
Brycyn <brycyng@gmail.com>
2023-12-05 15:42:40 -0800

    no duplicat rsvp

--- eventPlanner/planner/models.py
+++ eventPlanner/planner/models.py
@@ -45,7 +45,9 @@ class RSVP(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE, default=None)
     task = models.ForeignKey(Task, on_delete=models.CASCADE, default=None, null=True)
     guests = models.IntegerField(default=0)
+    rsvp = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
 
